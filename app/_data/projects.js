ad1c2f611c731a579cbb25f0c5666ad5a43a0df9
abram-stack <anthonybramantyo@outlook.com>
2023-12-04 16:05:37 +0100

    project content update

--- app/_data/projects.js
+++ app/_data/projects.js
@@ -2,19 +2,19 @@
 export default [
   {
     id: 1,
-    title: 'movie list',
-    description: 'movie list something something. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur magni cum iste facilis nesciunt, consectetur inventore amet rem unde ducimus! ',
+    title: 'Favourite Movie List',
+    description: 'Hey there welcome to the Movie Time! With this app you can search your favourite movies, add it to your favourite list. The idea behind this project is quite simple, everyone must have remember most arguably the first app to build when they first start learn Web Development, ToDo list app. Well this is quite similar, however this app has more feature! Combining the core concept of basic JavaScript and of course HTML and CSS for styling, i created simple layout, yet covered the cool things what Web Dev beginners can do. On top of that i used API call, this call API has similar feature with IMDB. This API will return us the data, so that we can get the list of movies. ',
     imgSrc: '/movielist-proj.png',
-    highlight:'Weekend and chill? Add your favourite movies to the list! ',
+    highlight:'Its weekend and your trying to figure out what to watch? Use this web app to add your favourite movies to the list! ',
     tech: ['html', 'css', 'js', 'API', 'firebase'],
     website:'https://movielist-consolebotanist.netlify.app/'
   },
   {
     id: 2,
     title: 'Shopping List Lesecafe',
-    description: 'one centralized shopping list for the co-worker',
+    description: 'Now that i finished my study, I am focusing my self to go balistic on my frontend carrer/journey. I used to work as a part-time barista in one of the coziest cafe in the city where, i live. Some of ideas came up to me at some point while i was there. We always got the message on the group chat, that some of the stuffs were out, and one of us, mostly the one who had the next shift should get and bring^ them from the store. There was no way of keep tracking of our stuff. Well, there is a list, the problem is, it is on phyisical note on paper. You might have expected, if the list is somehow gone or not updated and many other possibilities. The idea of the app is, to have a centralized app, to keep track if something need to be refilled or bought from the store, and any co-worker could just add it to the list. Well in order to have the feature that any other user can have the access to your app or data is using external database. LocalStorage is not enough. We can add a free tier database from Google Firebase to our application. Okay, sounds good already, but how about the user, in this case the co-workers get the data in realtime. With Firebase Firestore Realtime DB, our data in DB such as the groceries list, will be added or remove in the real time manners. To make this things work, This app will be available to the other co-worker likewise. They only have to have the access to the link of this website. On top of that, i design this web app pretending to be a mobile app, meaning the it behave similar to the user for the mobile, without using the tech such as React Native. Well this app is far, far away from what you might want to call very profesional application, but you got the idea.',
     imgSrc: '/shoplist-proj.png',
-    highlight:'Not losing track of what you should buy from the store anymore! Add and remove to the basket easily.',
+    highlight:'A centralized shopping list for the co-worker. Add and remove to the basket easily.',
     tech: ['html', 'css', 'js', 'firebase'],
     website: 'https://cappucino-shopping-list.netlify.app/'
   },
